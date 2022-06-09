f0bd2a253a4303646e5c791ab65d9cf1e854f359
LuciferWin <luciferwin0@gmail.com>
2022-06-10 00:46:39 +0630

    Update 2

---
+++ my-custom/src/views/ContactView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Contact Page</h2>
+        <PartFile></PartFile>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+import PartFile from '@/components/PartFile.vue'
+export default {
+  name: 'AboutView',
+  components: {
+    PartFile,
+}
+}
+</script>
